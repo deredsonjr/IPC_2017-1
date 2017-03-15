cc8da3c7e849c864f3656d9c9a0a8800c3160aac
deredsonjr <edson.limajr@gmail.com>
2017-03-15 19:27:12 -0400

    Update lista1.5_questao21.py

--- lista1.5/lista1.5_questao21.py
+++ lista1.5/lista1.5_questao21.py
@@ -8,7 +8,10 @@
 # Tiago Ferreira Aranha 	                1715310047
 # Wilbert Luís Evangelista Marins           1715310055
 # Mackson Garcez Moreno de Oliveira júnior  1215090300
-#
-# 21.
+
+# 21.Faça um programa que receba o numro de horas trabalhadas, o valor do salario mínimo, e o numero de horas extras trabalhadas.
+#Calcule e mostre o salário a receber seguinda a regra a seguir:
+# a)a hora trabalhada vale 1/8 do salário minimo
+#o sa´lario a receber
 # ----------------------------------------------------------
 
